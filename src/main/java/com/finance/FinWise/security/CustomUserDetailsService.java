3e6588337219f090a3c03a4b0aebb03c98fe2738
kfoo0012 <kfoo0012@student.monash.edu>
2025-03-19 16:16:51 +0800

    Implement JWT

--- src/main/java/com/finance/FinWise/security/CustomUserDetailsService.java
+++ src/main/java/com/finance/FinWise/security/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.finance.FinWise.security;
 
-public class CustomUserDetailsService {
+import com.finance.FinWise.model.User;
+import com.finance.FinWise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUserEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserEmail(), user.getUserPassword(), user.getAuthorities());
+    }
 }
